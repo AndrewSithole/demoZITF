fc49045079b3de2745ea08ef85b1244b753f2135
AndrewSithole <Andrew@macs-MacBook-Air-2.local>
2016-04-07 00:24:35 +0200

    Map not working

---
+++ app/src/main/java/de/andrew/demoZITF/myRealmObjects/Information.java
@@ -0,0 +1,31 @@
+package de.andrew.demoZITF.myRealmObjects;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by Andrew on 3/29/16.
+ */
+public class Information extends RealmObject {
+
+    private String question;
+    private String answer;
+
+    public Information() { }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+}
---
+++ app/src/main/java/de/andrew/demoZITF/ui/quote/ArticleDetailActivity.java
@@ -0,0 +1,33 @@
+package de.andrew.demoZITF.ui.quote;
+
+import android.os.Bundle;
+
+import de.andrew.demoZITF.R;
+import de.andrew.demoZITF.ui.base.BaseActivity;
+
+/**
+ * Simple wrapper for {@link ArticleDetailFragment}
+ * This wrapper is only used in single pan mode (= on smartphones)
+ * Created by Andreas Schrade on 14.12.2015.
+ */
+public class ArticleDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        // Show the Up button in the action bar.
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        ArticleDetailFragment fragment =  ArticleDetailFragment.newInstance(getIntent().getStringExtra(ArticleDetailFragment.ARG_ITEM_ID));
+        getFragmentManager().beginTransaction().replace(R.id.article_detail_container, fragment).commit();
+    }
+
+    @Override
+    public boolean providesActivityToolbar() {
+        return false;
+    }
+}
